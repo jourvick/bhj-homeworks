df7fb79ec55da99afb406c54552550138bb03c5d
Viktoria <zheleznova.vicka17@gmail.com>
2026-02-28 20:45:45 +0300

    задача 2 решена

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,1 +1,15 @@
-// тест 
+const image = document.getElementById("cookie");
+const counterText = document.getElementById("clicker__counter");
+let counterNumber = Number(counterText.textContent);
+
+image.onclick = function() {
+	if (image.width === 200) {
+		image.width = 250;
+
+	} else {
+		image.width = 200;
+
+	}
+	counterNumber += 1;
+	counterText.textContent = counterNumber;
+}
