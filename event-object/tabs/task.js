e0c795c2155f681ed7d38b002141126d7d67d087
Viktoria <zheleznova.vicka17@gmail.com>
2026-03-06 19:57:58 +0300

    task 3

---
+++ event-object/tabs/task.js
@@ -0,0 +1,15 @@
+const tabsContainers = document.querySelectorAll('.tabs');
+
+tabsContainers.forEach(tb => {
+  const tabs = Array.from(tb.querySelectorAll('.tab'));
+  const contents = Array.from(tb.querySelectorAll('.tab__content'));
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+	  tabs.forEach(t => t.classList.remove('tab_active'));
+	  tab.classList.add('tab_active');
+	  contents.forEach(c => c.classList.remove('tab__content_active'));
+	  contents[index].classList.add('tab__content_active');
+	});
+  });
+});
